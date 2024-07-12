6b9c5502d24ea22cd07ccf18acb098ee91e094f5
Tobias A. Rau <github@tar-it.com>
2024-07-12 13:30:06 +0200

    Add config file; Outsource build function

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+/*
+* This is the configuration file. You can change the values to fit your needs.
+ */
+
+/* Directory Paths */
+ $assetsDir  = __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'assets';
+ $pagesDir  = __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'pages';
+ $includesDir = __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'includes';
+ $templatesDir = __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'templates';
+ $outDir = __DIR__ . DIRECTORY_SEPARATOR . 'public';
